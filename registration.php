61a3eb026283b9515ceee909e60d53e66dc33a7e
Charles Brunet <charles@cbrunet.net>
2017-01-30 20:58:14 -0500

    Modified to fr-ca language.

--- registration.php
+++ registration.php
@@ -1,18 +1,19 @@
 <?php
 
 /**
- * Magento2 French Language Pack (fr_FR)
- *
+ * Magento2 French Language Pack (fr_CA)
+ * 
  * Website: http://imaginaerum.github.io/magento2-language-fr-fr
- * Github: https://github.com/Imaginaerum/magento2-language-fr-fr
- * Packagist: https://packagist.org/packages/imaginaerum/magento2-language-fr-fr
- *
+ * Github: https://github.com/cbrunet/magento2-language-fr-ca
+ * Packagist: https://packagist.org/packages/cbrunet/magento2-language-fr-ca
+ * 
  * @author Sylvain Kocet <pyksel@gmail.com>
+ * @author Charles Brunet <charles@cbrunet.net>
  * @copyright Copyright © 2016 Imaginaerum
- * @version 100.6.23
+ * @version 100.7.00
  */
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'imaginaerum_fr_fr',
+    'cbrunet_fr_ca',
     __DIR__
 );
